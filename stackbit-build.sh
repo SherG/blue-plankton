62af72f1de019029c1e065654a8e973c8e1831d5
sherrie gossett <sherrie.g5@gmail.com>
2023-07-24 12:32:27 -0400

    Update stackbit-build.sh

--- stackbit-build.sh
+++ stackbit-build.sh
@@ -4,10 +4,14 @@ set -e
 set -o pipefail
 set -v
 
-if [[ -z "${STACKBIT_API_KEY}" ]]; then
-    echo "WARNING: No STACKBIT_API_KEY environment variable set, skipping stackbit-pull"
-else
-    npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5d9266f71dc6f20011d02024 
-fi
-hugo
-./inject-netlify-identity-widget.js public
+initialGitHash=$(git rev-list --max-parents=0 HEAD)
+node ./studio-build.js $initialGitHash &
+
+curl -s -X POST https://api.stackbit.com/project/5db837090317cd0014b27aea/webhook/build/pull > /dev/null
+npx @stackbit/stackbit-pull --stackbit-pull-api-url=https://api.stackbit.com/pull/5db837090317cd0014b27aea
+curl -s -X POST https://api.stackbit.com/project/5db837090317cd0014b27aea/webhook/build/ssgbuild > /dev/null
+gatsby build
+wait
+
+curl -s -X POST https://api.stackbit.com/project/5db837090317cd0014b27aea/webhook/build/publish > /dev/null
+echo "Stackbit-build.sh finished build"
